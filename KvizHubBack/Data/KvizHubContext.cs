69e23fa0d95ef836022203e56ecd6c1795848983
v_vilimanovic <vilimanovicvojin@gmail.com>
2025-08-31 19:17:49 +0200

    Creation of KvizHubContext for generating a db context for backend to use.

---
+++ KvizHubBack/Data/KvizHubContext.cs
@@ -0,0 +1,21 @@
+﻿using KvizHubBack.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KvizHubBack.Data
+{
+    public class KvizHubContext : DbContext
+    {
+        public KvizHubContext(DbContextOptions<KvizHubContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Quiz> Quizzes { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // opcionalno fluent API konfiguracije
+        }
+    }
+}
