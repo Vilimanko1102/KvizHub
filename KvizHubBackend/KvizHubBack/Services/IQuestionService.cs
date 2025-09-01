3d3fdcb8143280f66ca36b2a154cdc11130e26f9
v_vilimanovic <vilimanovicvojin@gmail.com>
2025-09-01 04:22:25 +0200

    Creation of basic service class for question entity.

---
+++ KvizHubBackend/KvizHubBack/Services/IQuestionService.cs
@@ -0,0 +1,14 @@
+﻿using KvizHubBack.DTOs.KvizHubBack.DTOs.Question;
+using KvizHubBack.DTOs.Question;
+
+namespace KvizHubBack.Services
+{
+    public interface IQuestionService
+    {
+        QuestionDto CreateQuestion(QuestionCreateDto dto);
+        IEnumerable<QuestionDto> GetAllQuestions();
+        QuestionDto GetQuestionById(int id);
+        QuestionDto UpdateQuestion(int id, QuestionUpdateDto dto);
+        void DeleteQuestion(int id);
+    }
+}
---
+++ KvizHubBackend/KvizHubBack/Services/QuestionService.cs
@@ -0,0 +1,72 @@
+﻿using KvizHubBack.DTOs.KvizHubBack.DTOs.Question;
+using KvizHubBack.DTOs.Question;
+using KvizHubBack.Models;
+using KvizHubBack.Repositories;
+
+namespace KvizHubBack.Services
+{
+    public class QuestionService : IQuestionService
+    {
+        private readonly IQuestionRepository _repo;
+
+        public QuestionService(IQuestionRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public QuestionDto CreateQuestion(QuestionCreateDto dto)
+        {
+            var question = new Question
+            {
+                Text = dto.Text,
+                QuizId = dto.QuizId
+            };
+
+            _repo.Add(question);
+
+            return new QuestionDto
+            {
+                Id = question.Id,
+                Text = question.Text,
+                QuizId = question.QuizId
+            };
+        }
+
+        public IEnumerable<QuestionDto> GetAllQuestions()
+        {
+            return _repo.GetAll().Select(q => new QuestionDto
+            {
+                Id = q.Id,
+                Text = q.Text,
+                QuizId = q.QuizId
+            });
+        }
+
+        public QuestionDto GetQuestionById(int id)
+        {
+            var q = _repo.GetById(id);
+            if (q == null) throw new KeyNotFoundException();
+            return new QuestionDto { Id = q.Id, Text = q.Text, QuizId = q.QuizId };
+        }
+
+        public QuestionDto UpdateQuestion(int id, QuestionUpdateDto dto)
+        {
+            var q = _repo.GetById(id);
+            if (q == null) throw new KeyNotFoundException();
+
+            q.Text = dto.Text;
+            q.QuizId = dto.QuizId;
+
+            _repo.Update(q);
+
+            return new QuestionDto { Id = q.Id, Text = q.Text, QuizId = q.QuizId };
+        }
+
+        public void DeleteQuestion(int id)
+        {
+            var q = _repo.GetById(id);
+            if (q == null) throw new KeyNotFoundException();
+            _repo.Delete(q);
+        }
+    }
+}
