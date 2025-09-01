a0e666f418d1d5716fa9dc626a6797e27091e39c
v_vilimanovic <vilimanovicvojin@gmail.com>
2025-09-01 04:30:29 +0200

    Creation of basic repository class for answer entity.

---
+++ KvizHubBackend/KvizHubBack/Repositories/AnswerRepository.cs
@@ -0,0 +1,37 @@
+﻿using KvizHubBack.Data;
+using KvizHubBack.Models;
+
+namespace KvizHubBack.Repositories
+{
+    public class AnswerRepository : IAnswerRepository
+    {
+        private readonly KvizHubContext _context;
+
+        public AnswerRepository(KvizHubContext context)
+        {
+            _context = context;
+        }
+
+        public Answer GetById(int id) => _context.Answers.FirstOrDefault(a => a.Id == id);
+
+        public IEnumerable<Answer> GetAll() => _context.Answers.ToList();
+
+        public void Add(Answer answer)
+        {
+            _context.Answers.Add(answer);
+            _context.SaveChanges();
+        }
+
+        public void Update(Answer answer)
+        {
+            _context.Answers.Update(answer);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Answer answer)
+        {
+            _context.Answers.Remove(answer);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ KvizHubBackend/KvizHubBack/Repositories/IAnswerRepository.cs
@@ -0,0 +1,13 @@
+﻿using KvizHubBack.Models;
+
+namespace KvizHubBack.Repositories
+{
+    public interface IAnswerRepository
+    {
+        Answer GetById(int id);
+        IEnumerable<Answer> GetAll();
+        void Add(Answer answer);
+        void Update(Answer answer);
+        void Delete(Answer answer);
+    }
+}
