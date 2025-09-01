4d3e86cf1fd9a33982e70a808dd68abb2e1e068a
v_vilimanovic <vilimanovicvojin@gmail.com>
2025-09-01 04:31:48 +0200

    Creation of basic service class for answer entity.

---
+++ KvizHubBackend/KvizHubBack/Services/AnswerService.cs
@@ -0,0 +1,75 @@
+﻿using KvizHubBack.DTOs.Answer;
+using KvizHubBack.Models;
+using KvizHubBack.Repositories;
+
+namespace KvizHubBack.Services
+{
+    public class AnswerService : IAnswerService
+    {
+        private readonly IAnswerRepository _repo;
+
+        public AnswerService(IAnswerRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public AnswerDto CreateAnswer(AnswerCreateDto dto)
+        {
+            var answer = new Answer
+            {
+                Text = dto.Text,
+                IsCorrect = dto.IsCorrect,
+                QuestionId = dto.QuestionId
+            };
+
+            _repo.Add(answer);
+
+            return new AnswerDto
+            {
+                Id = answer.Id,
+                Text = answer.Text,
+                IsCorrect = answer.IsCorrect,
+                QuestionId = answer.QuestionId
+            };
+        }
+
+        public IEnumerable<AnswerDto> GetAllAnswers()
+        {
+            return _repo.GetAll().Select(a => new AnswerDto
+            {
+                Id = a.Id,
+                Text = a.Text,
+                IsCorrect = a.IsCorrect,
+                QuestionId = a.QuestionId
+            });
+        }
+
+        public AnswerDto GetAnswerById(int id)
+        {
+            var a = _repo.GetById(id);
+            if (a == null) throw new KeyNotFoundException();
+            return new AnswerDto { Id = a.Id, Text = a.Text, IsCorrect = a.IsCorrect, QuestionId = a.QuestionId };
+        }
+
+        public AnswerDto UpdateAnswer(int id, AnswerUpdateDto dto)
+        {
+            var a = _repo.GetById(id);
+            if (a == null) throw new KeyNotFoundException();
+
+            a.Text = dto.Text;
+            a.IsCorrect = dto.IsCorrect;
+            a.QuestionId = dto.QuestionId;
+
+            _repo.Update(a);
+
+            return new AnswerDto { Id = a.Id, Text = a.Text, IsCorrect = a.IsCorrect, QuestionId = a.QuestionId };
+        }
+
+        public void DeleteAnswer(int id)
+        {
+            var a = _repo.GetById(id);
+            if (a == null) throw new KeyNotFoundException();
+            _repo.Delete(a);
+        }
+    }
+}
---
+++ KvizHubBackend/KvizHubBack/Services/IAnswerService.cs
@@ -0,0 +1,13 @@
+﻿using KvizHubBack.DTOs.Answer;
+
+namespace KvizHubBack.Services
+{
+    public interface IAnswerService
+    {
+        AnswerDto CreateAnswer(AnswerCreateDto dto);
+        IEnumerable<AnswerDto> GetAllAnswers();
+        AnswerDto GetAnswerById(int id);
+        AnswerDto UpdateAnswer(int id, AnswerUpdateDto dto);
+        void DeleteAnswer(int id);
+    }
+}
