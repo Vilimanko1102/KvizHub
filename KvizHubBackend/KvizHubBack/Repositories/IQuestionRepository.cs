27408030de9266a71fbd17135d4f5cd5fde8a945
v_vilimanovic <vilimanovicvojin@gmail.com>
2025-09-01 04:19:57 +0200

    Creation of basic repository class for question entity.

---
+++ KvizHubBackend/KvizHubBack/Repositories/IQuestionRepository.cs
@@ -0,0 +1,13 @@
+﻿using KvizHubBack.Models;
+
+namespace KvizHubBack.Repositories
+{
+    public interface IQuestionRepository
+    {
+        Question GetById(int id);
+        IEnumerable<Question> GetAll();
+        void Add(Question question);
+        void Update(Question question);
+        void Delete(Question question);
+    }
+}
---
+++ KvizHubBackend/KvizHubBack/Repositories/QuestionRepository.cs
@@ -0,0 +1,37 @@
+﻿using KvizHubBack.Data;
+using KvizHubBack.Models;
+
+namespace KvizHubBack.Repositories
+{
+    public class QuestionRepository : IQuestionRepository
+    {
+        private readonly KvizHubContext _context;
+
+        public QuestionRepository(KvizHubContext context)
+        {
+            _context = context;
+        }
+
+        public Question GetById(int id) => _context.Questions.FirstOrDefault(q => q.Id == id);
+
+        public IEnumerable<Question> GetAll() => _context.Questions.ToList();
+
+        public void Add(Question question)
+        {
+            _context.Questions.Add(question);
+            _context.SaveChanges();
+        }
+
+        public void Update(Question question)
+        {
+            _context.Questions.Update(question);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Question question)
+        {
+            _context.Questions.Remove(question);
+            _context.SaveChanges();
+        }
+    }
+}
